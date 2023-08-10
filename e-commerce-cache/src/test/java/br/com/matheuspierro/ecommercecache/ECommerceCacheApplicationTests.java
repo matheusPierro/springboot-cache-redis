966c158006e4b1a7a2268694cbe0b061ebc6d45a
matheusPierro <matpierro570@gmail.com>
2023-08-10 12:08:24 -0300

    Inserindo projeto criado sem a integração do redis para tratamento de cache, também será criada uma branch onde você poderá testar esse projeto antes da implementação da memória em cache

---
+++ e-commerce-cache/src/test/java/br/com/matheuspierro/ecommercecache/ECommerceCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheuspierro.ecommercecache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
