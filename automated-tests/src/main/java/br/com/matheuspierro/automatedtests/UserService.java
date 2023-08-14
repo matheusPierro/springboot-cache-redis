46ec6704e5a2aa0a1db01d693c07c48aeb95e19c
matheusPierro <matpierro570@gmail.com>
2023-08-14 20:43:00 -0300

    UserService adaptation to pass the second test scenario

--- automated-tests/src/main/java/br/com/matheuspierro/automatedtests/UserService.java
+++ automated-tests/src/main/java/br/com/matheuspierro/automatedtests/UserService.java
@@ -10,6 +10,8 @@ public class UserService {
     List<User> users = new ArrayList<>();
 
     public List<User> create(User user) {
+        if (users.contains(user))
+            throw new RuntimeException();// Not the best solution... just for a moment
         users.add(user);
         return users;
     }
