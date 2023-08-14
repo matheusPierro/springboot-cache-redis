38ee4fd63dbbc34b0301441c38e848daa55ca799
matheusPierro <matpierro570@gmail.com>
2023-08-13 23:10:26 -0300

    Class that makes the get request sending a product to the kafka topic

--- e-commerce-messaging/src/main/java/br/com/matheuspierro/ecommercemessaging/ECommerceMessagingApplication.java
+++ e-commerce-messaging/src/main/java/br/com/matheuspierro/ecommercemessaging/ECommerceMessagingApplication.java
@@ -2,6 +2,10 @@ package br.com.matheuspierro.ecommercemessaging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ECommerceMessagingApplication {
@@ -10,4 +14,20 @@ public class ECommerceMessagingApplication {
 		SpringApplication.run(ECommerceMessagingApplication.class, args);
 	}
 
+}
+
+@RestController
+@RequestMapping("/kafka")
+class ProductsController {
+	private ProductProducer service;
+
+	public ProductsController(ProductProducer service) {
+		this.service = service;
+	}
+
+	@GetMapping("/product/{name}")
+	public String product(@PathVariable("name") String name) {
+		service.sendProduct("Product: " + name);
+		return "OK";
+	}
 }
