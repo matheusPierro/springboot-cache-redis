8c0c18d237be96fd30bc826a8c4812950fe31807
matheusPierro <matpierro570@gmail.com>
2023-08-13 23:14:13 -0300

    Creating a project with spring security to protect routes

---
+++ e-commerce-security/src/main/java/br/com/matheuspierro/ecommercesecurity/ECommerceSecurityApplication.java
@@ -0,0 +1,15 @@
+package br.com.matheuspierro.ecommercesecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ECommerceSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceSecurityApplication.class, args);
+	}
+
+}
---
+++ e-commerce-security/src/test/java/br/com/matheuspierro/ecommercesecurity/ECommerceSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheuspierro.ecommercesecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
